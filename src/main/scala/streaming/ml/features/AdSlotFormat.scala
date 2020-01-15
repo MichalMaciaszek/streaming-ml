fdc230178fb8cdccfb2e3e9e383de1890eeb188e
margohanyz <malgorzata.hanyz@student.put.poznan.pl>
2020-01-15 17:59:16 +0000

    Upload AdSlotFormat feature

---
+++ src/main/scala/streaming/ml/features/AdSlotFormat.scala
@@ -0,0 +1,15 @@
+package streaming.ml.features
+
+import streaming.ml.RawEvent
+
+object AdSlotFormat extends Feature {
+
+  private val FORMATS = Vector("0", "1", "2")
+
+  override def size: Int = FORMATS.size
+
+  override def extract(event: RawEvent): (Int, Double) = {
+    val format = event.data("AdSlotFormat").toLowerCase
+    (FORMATS.map(format.contains).zipWithIndex.filter(_._1).map(_._2).headOption.getOrElse(size), 1.0)
+  }
+}
