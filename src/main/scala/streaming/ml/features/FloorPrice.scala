26a45b7477c598b7580434793ff16d799c9b8311
margohanyz <malgorzata.hanyz@student.put.poznan.pl>
2020-01-16 16:12:11 +0000

    Update FloorPrice.scala

--- src/main/scala/streaming/ml/features/FloorPrice.scala
+++ src/main/scala/streaming/ml/features/FloorPrice.scala
@@ -4,18 +4,18 @@ import streaming.ml.RawEvent
 
 object FloorPrice extends Feature {
 
-  private val PRICES = Vector(0, 1, 11, 51, 100)
+  private val PRICES = Vector(0, 11, 51, 100)
 
-  override def size: Int = PRICES.size
+  override def size: Int = PRICES.size + 1
 
   override def extract(event: RawEvent): (Int, Double) = {
     val price = event.data("AdSlotFloorPrice").toLowerCase.toInt
     val index = {
         if (price == PRICES(0)) 0 // [0]
-        else if (price >= PRICES(1) && price < PRICES(2)) 1 // [1-10]
-        else if (price >= PRICES(2) && price < PRICES(3)) 2 // [11-50]
-        else if (price >= PRICES(3) && price <= PRICES(4)) 3 // [51-100]
-        else 4 // [>100]
+        else if (price > PRICES(0) && price < PRICES(1)) 1 // [1-10]
+        else if (price >= PRICES(1) && price < PRICES(2)) 2 // [11-50]
+        else if (price >= PRICES(2) && price <= PRICES(3)) 3 // [51-100]
+        else 3 // [>100]
     }
     (index, 1.0)
   }
