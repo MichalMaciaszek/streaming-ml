9f87dd00b4dcff52d44782a5dc3468a42ded725e
margohanyz <malgorzata.hanyz@student.put.poznan.pl>
2020-01-22 22:24:52 +0000

    Update Day.scala

--- src/main/scala/streaming/ml/features/Day.scala
+++ src/main/scala/streaming/ml/features/Day.scala
@@ -8,6 +8,13 @@ object Day extends Feature {
 
   override def extract(event: RawEvent): (Int, Double) = {
     val hour = event.data("Timestamp").toLowerCase
-    (hour.substring(8,10).toInt % 6,1.0)
+    val pd = hour.substring(8,10).toInt % 6
+    val index = {
+        if (pd >= 0 && pd < 6) 0
+        else if (pd >= 6 && pd < 12) 1
+        else if (pd >= 12 && pd < 18) 2
+        else 3
+    }
+    (index, 1.0)
   }
 }
